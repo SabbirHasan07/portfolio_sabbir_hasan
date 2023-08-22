faf27e75b88c0267ea1ad5768fdb5ddf57db284f
reveals007 <sabbirhasan170698@gmail.com>
2023-08-22 19:02:12 +0600

    first commit

--- components/ProjectsBtn.js
+++ components/ProjectsBtn.js
@@ -1,5 +1,20 @@
+
+import Link from "next/link";
+import roundedImage from "../public/rounded-text.png";
+import { HiArrowRight } from "react-icons/hi2";
+import Image from "next/image";
 const ProjectsBtn = () => {
-  return <div>Project Btn</div>;
+  return <div className="mx-auto xl:mx-0">
+    <Link href={'/work'} className="relative w-[185px] h-[185px] flex justify-center items-center bg-circleStar bg-cover bg-centerbg-no-repeat grooup">
+      <Image
+      src={'/rounded-text.png'}
+     width={141}
+     height={148}
+     alt=""
+     className='animate-spin-slow w-full h-full max-w-[141px] max-h-[148px]'/>
+     <HiArrowRight className="absolute text-4xl group-hover:translate-x-2 transition-all duration-300"/>
+    </Link>
+  </div>
 };
 
 export default ProjectsBtn;
