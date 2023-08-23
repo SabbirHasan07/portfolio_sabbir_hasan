1bbfc7a266791952b1edf4117dbcb9e4f009c856
reveals007 <sabbirhasan170698@gmail.com>
2023-08-23 12:33:24 +0600

    last_update

--- components/Avatar.js
+++ components/Avatar.js
@@ -2,7 +2,7 @@ import Image from 'next/image';
 import avatar from '../public/avatar.png'
 
 const Avatar = () => {
-  return <div className='hidden xl:flex xl:max-w-none mt-36'>
+  return <div className='hidden xl:flex xl:max-w-none mt-28 mix-blend-color-dodge'>
     <Image
     src={avatar}
     height={500}
--- pages/about/index.js
+++ pages/about/index.js
@@ -1,3 +1,7 @@
+import React, { useState } from 'react'
+import { TbBrandNextjs } from "react-icons/tb";
+
+
 // icons
 import {
   FaHtml5,
@@ -12,6 +16,8 @@ import {
   SiNextdotjs,
   SiFramer,
   SiAdobexd,
+  SiC,
+  SiCplusplus,
   SiAdobephotoshop,
 } from "react-icons/si";
 
@@ -28,45 +34,30 @@ const aboutData = [
           <FaCss3 />,
           <FaJs />,
           <FaReact />,
-          <SiNextdotjs />,
-          <SiFramer />,
-          <FaWordpress />,
+          <TbBrandNextjs/>
+          
+          
         ],
       },
       {
-        title: 'UI/UX Design',
-        icons: [<FaFigma />, <SiAdobexd />, <SiAdobephotoshop />],
-      },
-    ],
-  },
-  {
-    title: 'awards',
-    info: [
-      {
-        title: 'Webby Awards - Honoree',
-        stage: '2011 - 2012',
+        title: 'Programming Skills',
+        icons: [<SiC/>, <SiCplusplus />, <SiAdobephotoshop />],
       },
       {
-        title: 'Adobe Design Achievement Awards - Finalist',
-        stage: '2009 - 2010',
-      },
+        title: 'Around 200+ problems solve in the differents online judge.'
+      }
     ],
   },
+ 
   {
     title: 'experience',
     info: [
+     
       {
-        title: 'UX/UI Designer - XYZ Company',
-        stage: '2012 - 2023',
+        title: 'Web Developer - Programming Hero',
+        stage: '2022(jun) - 2022(December)',
       },
-      {
-        title: 'Web Developer - ABC Agency',
-        stage: '2010 - 2012',
-      },
-      {
-        title: 'Intern - DEF Corporation',
-        stage: '2008 - 2010',
-      },
+      
     ],
   },
   {
@@ -86,10 +77,80 @@ const aboutData = [
       },
     ],
   },
+  {
+    title: 'awards',
+    info: [
+      {
+        title: 'Webby Awards - Honoree',
+        stage: '2011 - 2012',
+      },
+      {
+        title: 'Adobe Design Achievement Awards - Finalist',
+        stage: '2009 - 2010',
+      },
+    ],
+  },
 ];
 
+//components
+import Avatar from '../../components/Avatar';
+import Circles from '../../components/Circles';
+import Image from 'next/image';
+
 const About = () => {
-  return <div>About</div>;
+  const [index, setIndex] = useState(0);
+  return <div className='h-full bg-primary/30 py-32 text-center xl:text-left'>
+    <Circles />
+    <div className='hidden xl:flex absolute bottom-0 -left-[370px]'>
+      <Avatar />
+    </div>
+    <div className='container mx-auto h-full flex flex-col items-center xl:flex-grow gap-x-6'>
+      <div className='flex  justify-center xl:pb-8 '>
+        <div><Image
+          src={'/sabbir.jpeg'}
+          height={300}
+          width={300}
+          className='mb-2 rounded-md xl:h-48 xl:w-48'
+          alt=''
+        />
+        </div>
+        <p className='px-4 mt-0 justify-center text-center items-center xl:mt-16 text-white xl:text-2xl'> I have successfully completed 12 projects that <br /> showcase my proficiency and versatility.</p>
+      </div>
+      <div className='flex flex-col mb-9'>
+        <div className='flex gap-x-4 xl:gap-x-8 mx-auto xl:mx-0 mb-4'>
+          {aboutData.map((item, itemIndex) => {
+            return (
+              <div key={itemIndex}
+                className={`${index === itemIndex && 'text-accent after:w-[100%] after:bg-accent after:transition-all after:duration-300'} cusor-pointer capitalize xl:text-lg relative after:w-8 after:h-[2px] after:bg-white after:absolute after:-bottom-1 after:left-0 `}
+                onClick={() => setIndex(itemIndex)}
+              >
+                {item.title}
+              </div>
+            )
+          })}
+        </div>
+        <div className='  flex flex-col  items-center xl:items-start'>
+          {aboutData[index].info.map((item, itemIndex) => {
+            return (
+              <div key={itemIndex}>
+                <div>{item.title}</div>
+                <div className=' md:flex gap-2'>{item.stage}</div>
+                <div className='flex gap-x-4 py-2'>
+                  {item.icons?.map((icon, itemIndex) => {
+                    return <div key={itemIndex}
+                      className='gap-x-4 text-2xl'>
+                      {icon}
+                    </div>
+                  })}
+                </div>
+
+              </div>
+            )
+          })}
+        </div>
+      </div>
+    </div>
+  </div>;
 };
 
 export default About;
