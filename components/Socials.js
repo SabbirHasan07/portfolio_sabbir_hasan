96718a1c6f8aec7934001afc7d50827bc385ca78
reveals007 <sabbirhasan170698@gmail.com>
2023-08-28 19:37:57 +0600

    update

--- components/Socials.js
+++ components/Socials.js
@@ -6,8 +6,8 @@ const Socials = () => {
   return <div className='flex items-center gap-x-5 text-lg'>
     <Link href='https://github.com/SabbirHasan07' className=' hover:text-accent transition-all duration-300 text-2xl '><RiGithubFill/></Link>
     <Link href='https://www.linkedin.com/in/sabbir-hasan-a08b71205/' className=' hover:text-accent transition-all duration-300 text-2xl '><RiLinkedinBoxFill/></Link>
-    <Link href={''} className=' hover:text-accent transition-all duration-300 text-2xl'><RiFileTextFill/></Link>
-    <Link href={''} className=' hover:text-accent transition-all duration-300 text-2xl'><RiDownload2Fill/></Link>
+    <Link href='https://drive.google.com/file/d/1iektfaYliyCXGBNwgs9rlZgMZSx-UQCX/view?usp=sharing'  className=' hover:text-accent transition-all duration-300 text-2xl'><RiFileTextFill/></Link>
+    <Link href='https://drive.google.com/file/d/1gwsrsLpBlJuE7kv6WZ-hWD29t9LJSL36/view?usp=sharing' className=' hover:text-accent transition-all duration-300 text-2xl'><RiDownload2Fill/></Link>
   </div>;
 };
 
