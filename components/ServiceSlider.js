ccc9fcafeb4e90a00bfdeb36bc9a3118a62287b1
reveals007 <sabbirhasan170698@gmail.com>
2023-08-28 11:37:14 +0600

    update

--- components/ServiceSlider.js
+++ components/ServiceSlider.js
@@ -1,3 +1,8 @@
+import {Swiper,SwiperSlide} from 'swiper/react'
+import 'swiper/css';
+import 'swiper/css/free-mode'
+import 'swiper/css/pagination'
+
 // icons
 import {
   RxCrop,
@@ -5,14 +10,16 @@ import {
   RxDesktop,
   RxReader,
   RxRocket,
+  RxArrowTopRight
 } from "react-icons/rx";
-
+import { FreeMode,Pagination } from 'swiper';
 
 // data
 const serviceData = [
+  
   {
-    icon: <RxCrop />,
-    title: 'Branding',
+    icon: <RxDesktop />,
+    title: 'Development',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
   },
   {
@@ -21,24 +28,65 @@ const serviceData = [
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
   },
   {
-    icon: <RxDesktop />,
-    title: 'Development',
+    icon: <RxRocket />,
+    title: 'SEO',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
   },
+  {
+    icon: <RxCrop />,
+    title: 'Branding',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  },
+
   {
     icon: <RxReader />,
     title: 'Copywriting',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
   },
-  {
-    icon: <RxRocket />,
-    title: 'SEO',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-  },
+  ,
 ];
 
 const ServiceSlider = () => {
-  return <div>Service Slider</div>;
+  return <Swiper breakpoints={{
+    320:{
+      slidesPerView:1,
+      spaceBetween:15,
+    },
+    640:{
+      slidesPerView:3,
+      spaceBetween:15,
+    },
+  }}
+  freeMode={true}
+  pagination={{
+    clickable:true,
+  }}
+  modules={[FreeMode, Pagination]}
+  className='h-[280px] xl:h-[300px]'
+  
+  >
+    {
+      serviceData.map((item,index)=>{
+        return<SwiperSlide
+        key={index}
+        >
+          <div className='bg-[rgba(65,47,123,0.15)] h-max rounded-lg px-6 py-8 xl:flex sm:flex-col gap-x-6  group cursor-pointer hover:bg-[rgba(89,65,169,0.15)] transition-all'>
+            <div className='text-4xl text-accent mb-4'>
+              {item.icon}
+            </div>
+            <div>
+              <div>{item?.title}</div>
+              <p>{item?.description}</p>
+            </div>
+            <div className='text-3xl'>
+              <RxArrowTopRight className='group-hover:rotate-45 group-hover:text-accent transition-all duration-300'/>
+            </div>
+          </div>
+
+        </SwiperSlide>
+      })
+    }
+  </Swiper>;
 };
 
 export default ServiceSlider;
--- pages/about/index.js
+++ pages/about/index.js
@@ -9,14 +9,11 @@ import {
   FaCss3,
   FaJs,
   FaReact,
-  FaWordpress,
-  FaFigma,
 } from "react-icons/fa";
 
 import {
   SiC,
   SiCplusplus,
-  SiAdobephotoshop,
 } from "react-icons/si";
 
 
@@ -31,10 +28,10 @@ const aboutData = [
           <FaHtml5 key={''} />,
           <FaCss3 key={''} />,
           <FaJs key={''} />,
-          <FaReact key={''}  />,
+          <FaReact key={''} />,
           <TbBrandNextjs key={''} />
-          
-          
+
+
         ],
       },
       {
@@ -42,20 +39,20 @@ const aboutData = [
         icons: [<SiC key={''} />, <SiCplusplus key={''} />],
       },
       {
-        title: 'Around 200+ problems solve in the differents online judge.'
+        title: '# Around 200+ problems solve in the differents online judge.'
       }
     ],
   },
- 
+
   {
     title: 'experience',
     info: [
-     
+
       {
         title: 'Web Developer - Programming Hero',
         stage: '2022(jun) - 2022(December)',
       },
-      
+
     ],
   },
   {
@@ -78,17 +75,13 @@ const aboutData = [
         title: 'Programming Hero',
         stage: ' - 2022',
       },
-      {
-        title: 'Government Scholarship - class( V, Viii )',
-        stage: '2009 and 2011',
-      },
-      
+
     ],
   },
 ];
 
 //components
-import Avatar from '../../components/Avatar';
+
 import Circles from '../../components/Circles';
 import Image from 'next/image';
 
@@ -97,53 +90,64 @@ const About = () => {
   const [index, setIndex] = useState(0);
   return <div className='h-full bg-primary/30 py-32 text-center xl:text-left'>
     <Circles />
-    <div className='hidden xl:flex absolute bottom-0 -left-[370px]'>
-      <Avatar />
-    </div>
-    <div className='container mx-auto h-full flex flex-col items-center xl:flex-grow gap-x-6'>
-      <div className='flex  justify-center xl:pb-8 mb-6 xl:mb-0'>
-        <div><Image
+
+    <div className='container mx-auto h-full flex flex-col items-center xl:flex-grow gap-x-4 '>
+      <div className='flex  justify-center xl:pb-4 mb-4 xl:mb-0'>
+        <div className='border-2'><Image
           src={'/sabbir.jpeg'}
           height={300}
           width={300}
-          className='mb-2 rounded-md xl:h-48 xl:w-48'
+          className='h-36 w-56 xl:h-48 xl:w-48'
           alt=''
         />
         </div>
-        <p className=' px-4 mt-0 justify-center text-center items-center xl:mt-16 text-white xl:text-2xl'> I have successfully completed <CountUp className='text-accent text-2xl font-bold' start={0} end={12} duration={5}/> + projects that <br /> showcase my proficiency and versatility.</p>
+        <p className=' px-4 mt-0 justify-center text-center items-center xl:mt-16 text-white xl:text-2xl'> I have successfully completed <CountUp className='text-accent text-2xl font-bold' start={0} end={12} duration={5} /><span className='text-accent'>+</span> projects that <br /> showcase my proficiency and versatility.</p>
         <div></div>
       </div>
-      <div className='flex flex-col mb-9'>
-        <div className='flex gap-x-4 xl:gap-x-8 mx-auto xl:mx-0 mb-4'>
-          {aboutData.map((item, itemIndex) => {
-            return (
-              <div key={itemIndex}
-                className={`${index === itemIndex && 'text-accent after:w-[100%] after:bg-accent after:transition-all after:duration-300'} cusor-pointer capitalize xl:text-lg relative after:w-8 after:h-[2px] after:bg-white after:absolute after:-bottom-1 after:left-0 `}
-                onClick={() => setIndex(itemIndex)}
-              >
-                {item.title}
-              </div>
-            )
-          })}
-        </div>
-        <div className=' flex flex-col  items-center xl:items-start'>
-          {aboutData[index].info.map((item, itemIndex) => {
-            return (
-              <div key={itemIndex}>
-                <div>{item.title}</div>
-                <div className=' md:flex gap-2'>{item.stage}</div>
-                <div className='flex gap-x-4 py-2'>
-                  {item.icons?.map((icon, itemIndex) => {
-                    return <div key={itemIndex}
-                      className='gap-x-4 text-2xl'>
-                      {icon}
-                    </div>
-                  })}
-                </div>
+      <div className='xl:flex xl:gap-x-12'>
+        <div className='grid grid-cols-2 m-0 p-0 gap-x-3 border-2 pt-1 rounded-md mb-1 xl:border-0'>
+          <div>
+            <p className='text-white font-bold'>SATIESFIED <br /> CLIENTS </p>
+            <p><CountUp className='text-accent text-2xl font-bold' start={0} end={5} duration={6} /><span className='text-accent'>+</span> </p>
+          </div>
+          <div>
+            <p className='text-white font-bold'>PROFESSIONAL <br /> PROJECTS </p>
+            <p><CountUp className='text-accent text-2xl font-bold' start={0} end={2} duration={7} /><span className='text-accent'>+</span> </p>
+          </div>
 
-              </div>
-            )
-          })}
+        </div>
+        <div className='flex flex-col '>
+          <div className='flex gap-x-4 xl:gap-x-8 mx-auto xl:mx-0 mb-4'>
+            {aboutData.map((item, itemIndex) => {
+              return (
+                <div key={itemIndex}
+                  className={`${index === itemIndex && 'text-accent after:w-[100%] after:bg-accent after:transition-all after:duration-300'} cusor-pointer capitalize xl:text-lg relative after:w-8 after:h-[2px] after:bg-white after:absolute after:-bottom-1 after:left-0 `}
+                  onClick={() => setIndex(itemIndex)}
+                >
+                  {item.title}
+                </div>
+              )
+            })}
+          </div>
+          <div className=' flex flex-col  items-center xl:items-start'>
+            {aboutData[index].info.map((item, itemIndex) => {
+              return (
+                <div key={itemIndex}>
+                  <div>{item.title}</div>
+                  <div className=' md:flex gap-2'>{item.stage}</div>
+                  <div className='flex gap-x-4 py-1'>
+                    {item.icons?.map((icon, itemIndex) => {
+                      return <div key={itemIndex}
+                        className='gap-x-4 text-sm xl:text-2xl'>
+                        {icon}
+                      </div>
+                    })}
+                  </div>
+
+                </div>
+              )
+            })}
+          </div>
         </div>
       </div>
     </div>
